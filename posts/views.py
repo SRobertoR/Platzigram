9a924d5b813f79c1d2067692ea2921e39890402b
SRobertoR <gn_wolver@hotmail.com>
2021-10-08 10:07:32 -0500

    Correciones en url y un error de bucle solucionado

--- posts/views.py
+++ posts/views.py
@@ -1,44 +1,78 @@
 """Posts views"""
 
-"""Django"""
-#Se requiere de sesion para acceder a la url Y SE IMPORTA EN SETINGS
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+# Django
+# Django
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DetailView, ListView
 
-#forms
+# Forms
 from posts.forms import PostForm
 
 # Models
 from posts.models import Posts
 
 
+class PostsFeedView(LoginRequiredMixin, ListView):
+    """Return all published posts."""
+
+    template_name = 'posts/feed.html'
+    model = Posts
+    ordering = ('-created',)
+    paginate_by = 30
+    context_object_name = 'posts'
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    """Return post detail."""
+
+    template_name = 'posts/detail.html'
+    queryset = Posts.objects.all()
+    context_object_name = 'post'
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    """Create a new post."""
+
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
+
+    def get_context_data(self, **kwargs):
+        """Add user and profile to context."""
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
+
+
 #Esta requerido 
-@login_required
-def list_posts(request):
-    """List existing posts."""
-    posts = Posts.objects.all().order_by('-created')
+# @login_required
+# def list_posts(request):
+#     """List existing posts."""
+#     posts = Posts.objects.all().order_by('-created')
 
-    return render(request, 'posts/feed.html', {'posts': posts})
+#     return render(request, 'posts/feed.html', {'posts': posts})
 
 
-@login_required
-def create_post(request):
-    """Create new post view."""
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('feed')
+# @login_required
+# def create_post(request):
+#     """Create new post view."""
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('posts:feed')
 
-    else:
-        form = PostForm()
+#     else:
+#         form = PostForm()
 
-    return render(
-        request=request,
-        template_name='posts/new.html',
-        context={
-            'form': form,
-            'user': request.user,
-            'profile': request.user.profile
-        }
-    )
+#     return render(
+#         request=request,
+#         template_name='posts/new.html',
+#         context={
+#             'form': form,
+#             'user': request.user,
+#             'profile': request.user.profile
+#         }
+#     )
